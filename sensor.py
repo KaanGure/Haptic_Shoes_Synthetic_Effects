8370bdcd5f83a3d13a67ef585019d52c5c38c365
KaanGure <kaan.gure@mail.mcgill.ca>
2022-04-02 15:24:08 -0400

    added communication

--- sensor.py
+++ sensor.py
@@ -9,9 +9,10 @@ import pub
 
 # Create TOF sensor
 TOF = READ_VL6180.createTOF()
-
+i = 0
 while True:
-
+    if i > 5:
+        i = 0
     # Read distance value from TOF
     dist = TOF.get_distance()
 
@@ -20,11 +21,14 @@ while True:
 
     #print(newDist)
     print(dist)
+    vel = [0 for i in range(5)]
+    vel[i] = dist
 
-    # Send converted value
-    #tiles[20:24] = newDist
-    #client.send_message("/niw/client", tiles)
+    velocity = sum(vel)/0.05
 
-    # Broadcast actual value read from TOF to ZMQ platform
-    #pub.sendTOFdata(socket, dist)
+    f = open("distance_velocity.txt", "w")
+    f.write(str(dist) + ";\n" + str(velocity) + ";\n")
+    f.close()
+    f = open("distance_velocity.txt", "r")
+    print(f.read())
     time.sleep(0.01)
